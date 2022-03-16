e569ca586b059e5449f7651b7653b781534a57d1
chiench <quangminhb53@gmail.com>
2022-03-16 08:43:27 +0700

    update

--- resources/views/livewire/detailcomponent.blade.php
+++ resources/views/livewire/detailcomponent.blade.php
@@ -115,7 +115,8 @@
                                 </div>
                             </div>
                             <div class="wrap-butons">
-                                <a href="#" wire:click.prevent="store({{$proDetails->id}},'{{$proDetails->name}}', {{$proDetails->regular_price}})" class="btn add-to-cart">Add to Cart</a>
+
+                                <a href="#" class="btn add-to-cart" wire:click.prevent="store({{$proDetails->id}},'{{$proDetails->name}}', {{$proDetails->regular_price}})">Add to Cart</a>
                                 <div class="wrap-btn">
                                     <a href="#" class="btn btn-compare">Add Compare</a>
                                     <a href="#" class="btn btn-wishlist">Add Wishlist</a>
